c81567926708a4214de5a3b93f0579de958aa29c
Jiayuan Zhang <forrestchang7@gmail.com>
2016-04-22 09:14:51 +0800

    refactor(Calculator): rewrite the calculator with MVC

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,90 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by 张佳圆 on 4/21/16.
+//  Copyright © 2016 Tisoga. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return "\(symbol)"
+                case .BinaryOperation(let symbol, _):
+                    return "\(symbol)"
+                }
+            }
+        }
+    }
+    
+    var opStack = [Op]()
+    var knownOps = [String: Op]()
+    
+    init() {
+        
+        func learnOp(op: Op) {
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−", { $1 - $0 }))
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷", { $1 / $0 }))
+        learnOp(Op.UnaryOperation("√", sqrt))
+        
+    }
+    
+    func eval(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = eval(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = eval(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = eval(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = eval(opStack)
+        print("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performanceOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -13,32 +13,65 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var performanceDisplay: UILabel!
     
+    var brain = CalculatorBrain()
+    
     var typedNumber = ""
     var hasTyped = false
-
+    
     @IBAction func appendDigit(sender: UIButton) {
         let numberInput = sender.currentTitle!
         typedNumber = "\(typedNumber)\(numberInput)"
         if hasTyped {
-            display.text = display.text! + sender.currentTitle!
+            display.text = display.text! + numberInput
         } else {
-            display.text = sender.currentTitle!
+            display.text = numberInput
             hasTyped = true
         }
+        
+        // performance display
+//        performanceDisplay.text = "\(performanceDisplay.text!) \(numberInput)"
     }
     
     
     @IBAction func operate(sender: UIButton) {
-        hasTyped = false
-        performanceDisplay.text = typedNumber + " " + sender.currentTitle!
-        typedNumber = ""
+        //hasTyped = false
+        if hasTyped {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performanceOperation(operation) {
+                displayNumber = result
+            } else {
+                displayNumber = 0
+            }
+        }
+        
+//        performanceDisplay.text = performanceDisplay.text! + " " + sender.currentTitle!
+//        typedNumber = ""
     }
     
     
     @IBAction func enter() {
         hasTyped = false
-        typedNumber = ""
+        if let result = brain.pushOperand(displayNumber) {
+            displayNumber = result
+        } else {
+            displayNumber = 0
+        }
+        
+        
+//        typedNumber = ""
     }
     
-
+    var displayNumber: Double {
+        set {
+            display.text = "\(newValue)"
+            hasTyped = false
+        }
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+    }
+    
 }
